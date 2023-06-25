ef5db066756ebdbf7eec5085c58092e2b34fc06e
ratemo <ratemolewis@gmail.ccom>
2023-06-25 19:10:25 +0300

    Authenticating with the Internet Computer

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,12 +1,31 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react'
 import App from "./components/App";
+import { AuthClient } from '@dfinity/auth-client';
+
 
 const init = async () => { 
   // ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
+
+const authClient = await AuthClient.create();
+
+if(authClient.isAuthenticated()){
+  handeleAuthenticated(authClient);
+}else{
+  await authClient.login({
+    identityProvider : "https://identity.ic0.app/#authorize",
+    onSuccess : () => {
+      handeleAuthenticated(authClient);
+    }
+  });
+}
+}
+
+async  function handeleAuthenticated(authClient){
+  await ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 }
 
 init();
